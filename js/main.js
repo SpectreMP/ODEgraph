f9c6c1353d12e1fa5df2b3b3dc90dcad2b691aa8
Mikhail <pms_03@mail.ru>
2024-02-24 03:18:13 +0800

    step function rewrite

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-function calculateNextStep(start, a, b, g, dt){
+function f(g, y, dy, a, b){
+    return b*g - a*y + dy;
+}
+function rungeKuttaStep(start, a, b, g, dt){
     n = start.length;
-    start.push(0);
 
     result = [];
     y = start[0] + b[n] * g;
 
     k1 = []; k2 = []; k3 = []; k4 = [];
     for (let i = 1; i<=n; i++){
-        k1[i] = dt * (b[n-i] * g - a[n-i] * y + start[i]);
-        k2[i] = dt * (b[n-i] * g - a[n-i] * (y + k1[i] / 2) + start[i]);
-        k3[i] = dt * (b[n-i] * g - a[n-i] * (y + k2[i] / 2) + start[i]);
-        k4[i] = dt * (b[n-i] * g - a[n-i] * (y + k3[i]) + start[i]);
-        
-        result.push(start[i-1]+(1/6)*(k1[i] + 2*k2[i] + 2*k3[i] + k4[i]))
-    }
-    
+        k1[i] = dt * f(g, y, i==n? 0: start[i], a[n-i], b[n-i]);
+    };
+    for (let i = 1; i<=n; i++){
+        k2[i] = dt * f(g, y + k1[1]/2, i==n? 0: start[i] + k1[i+1] / 2, a[n-i], b[n-i]);
+    };
+    for (let i = 1; i<=n; i++){
+        k3[i] = dt * f(g, y + k2[1]/2, i==n? 0: start[i] + k2[i+1] / 2, a[n-i], b[n-i]);
+    };
+    for (let i = 1; i<=n; i++){
+        k4[i] = dt * f(g, y + k3[1], i==n? 0: start[i] + k3[i+1], a[n-i], b[n-i]);  
+    };
+    for (let i = 1; i<=n; i++){
+        result.push(start[i-1]+(1/6)*(k1[i] + 2*k2[i] + 2*k3[i] + k4[i]));
+    };
     return result;
 }
 
@@ -50,7 +58,7 @@ function main(){
     while (t < L) {
         dy = y[0];
 
-        y = calculateNextStep(y, a, b, 1, dt);
+        y = rungeKuttaStep(y, a, b, 1, dt);
         dy = y[0] - dy;
 
         A[i] = [t, y[0], dy/dt];
